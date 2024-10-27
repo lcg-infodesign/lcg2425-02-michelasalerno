aadfca4e2acabff4c0d4e476540cff2551d26d76
michelasalerno <michela.salerno@mail.polimi.it>
2024-10-27 11:23:01 +0100

    Primo caricamento js modificato
    Scarabocchi con noise

--- sketch.js
+++ sketch.js
@@ -1,17 +1,46 @@
-function preload() {
-  // put preload code here
-}
-
+let positions = []; // Matrice vuota per le posizioni dei punti
+let side = 40; //distanza tra i punti di partenza
+let maxTracciaLength = 2000; // Lunghezza massima della traccia
+let staticFrame = 200; // Valore statico per simulare un determinato frame
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  // put setup code here
-  const message =
-    "This is a template repository\nfor the course Laboratorio di Computergrafica\nCommunication Design, Politecnico di Milano";
-  textAlign(CENTER, CENTER);
-  textSize(16);
-  text(message, width / 2, height / 2);
+  background(220);
+  let nPuntiX = ceil(windowWidth/side); // Numero di punti della griglia in X
+  let nPuntiY = ceil(windowHeight/side); // Numero di punti della griglia in Y
+  noiseSeed(99); //per far venire sempre gli stessi scarabocchi
+  
+  // inizio a creare la lista vuota che riempirò poi
+  for (let i = 0; i < nPuntiX; i++) {
+    positions[i] = [];
+    for (let j = 0; j < nPuntiY; j++) {
+      positions[i][j] = []; // lista per la traccia di ogni punto
+
+      // trovo i valori con cui riempire la lista
+      for (let t = 0; t < maxTracciaLength; t++) {
+        let noiseX = noise(0.005 * (staticFrame - t) + i * 0.1, j * 0.1);  
+        let noiseY = noise(0.005 * (staticFrame - t) + i * 0.1, j * 0.1 + 100);  
+        let x = i * side + noiseX * 50; //trovo coordinata x del punto
+        let y = j * side + noiseY * 50; // trovo coordinata y 
+        
+        // riempio la lista
+        positions[i][j].push(createVector(x, y));
+      }
+
+      // Disegna la traccia statica
+      drawTraccia(positions[i][j]);
+    }
+  }
 }
 
-function draw() {
-  // put drawing code here
+function drawTraccia(traccia) {
+  // Disegna le linee che compongono la traccia
+  noFill();
+  stroke(0);
+  strokeWeight(1.5);
+
+  beginShape();
+  for (let v of traccia) {
+    vertex(v.x, v.y);
+  }
+  endShape();
 }
